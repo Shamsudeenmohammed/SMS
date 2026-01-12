770ae2201e6755f597995caf033271a6bf2c520c
mohammedshamsudeen310-art <mohammedshamsudeen310@gmail.com>
2026-01-12 00:02:59 +0000

    add latest project updates

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,9 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.home, name='home'),
+
+    path('about/', views.about_view, name='about'),
+    path('admissions/', views.admissions_view, name='admissions'),
+    path('academics/', views.academics_view, name='academics'),
+    path('contact/', views.contact_view, name='contact'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from core.models import SiteUpdate
 from accounts.models import Student, Teacher, Parent, CustomUser
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
+from .forms import ContactForm
 
 def home(request):
     latest_update = SiteUpdate.objects.filter(is_active=True).first()
@@ -13,3 +16,59 @@ def home(request):
         "total_users": CustomUser.objects.count(),
     }
     return render(request, "core/home.html", context)
+
+
+from django.shortcuts import render
+
+def about_view(request):
+    return render(request, 'core/about.html')
+
+def admissions_view(request):
+    return render(request, 'core/admissions.html')
+
+def academics_view(request):
+    return render(request, 'core/academics.html')
+
+def contact_view(request):
+    return render(request, 'core/contact.html')
+
+
+
+def contact_view(request):
+    success = False
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+
+            full_message = f"""
+            New Contact Message
+
+            Name: {name}
+            Email: {email}
+
+            Message:
+            {message}
+            """
+
+            send_mail(
+                subject=f"[School Website] {subject}",
+                message=full_message,
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                recipient_list=[settings.DEFAULT_FROM_EMAIL],
+                fail_silently=False,
+            )
+
+            success = True
+            form = ContactForm()  # Clear form after success
+    else:
+        form = ContactForm()
+
+    return render(request, 'core/contact.html', {
+        'form': form,
+        'success': success
+    })
