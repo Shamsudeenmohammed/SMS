88ea3050718e9d1a5262e1cfabe0d5ec1856a16d
mohammedshamsudeen310-art <mohammedshamsudeen310@gmail.com>
2026-01-23 22:58:58 +0000

    add all latest project updates

--- SMS/settings.py
+++ SMS/settings.py
@@ -121,7 +121,7 @@ LOGIN_REDIRECT_URL = '/accounts/dashboard/'
 # 📧 Email Configuration
 # -----------------------------------------
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = "mohammedshamsudeen310@gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("mohammedshamsudeen310@gmail.com")
